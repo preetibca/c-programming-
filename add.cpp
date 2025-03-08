84cb952b48b1142f4126335b2d29ed58edf9362b
Preeti <preeti159080@gmail.com>
2025-03-08 15:02:28 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sub;
+    
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  
+  sub = first_number - second_number;
+  cout << first_number << " - " <<  second_number << " = " << sub;     
+
+  return 0;
+}
+
+
+
+
