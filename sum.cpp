1380f26027f10bc1e9dda7471fecf5d452094c18
Preeti <preeti159080@gmail.com>
2025-03-24 15:44:35 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,sum;
+	cout<<"Enter first number:";
+	cin>>a;
+	cout<<"Enter second number:";
+	cin>>b;
+	cout<<"Enter third number:";
+	cin>>c;
+	cout<<"Enter fourth number:";
+	cin>>d;
+	sum=a+b+c+d;
+	cout<<"sum ="<<sum;
+	return 0;
+	
+}
